03b5a0abd2ac49c050b5f85fa30015b0c022fc47
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2026-05-08 13:51:36 -0300

    Update app_view.py

--- app/views/app_view.py
+++ app/views/app_view.py
@@ -5,8 +5,8 @@ import os
 # Configuração da página (deve ser a primeira coisa do Streamlit)
 st.set_page_config(page_title="Biblioteca Digital")
 
-# --- A VERIFICAÇÃO CORRETA ---
-# O segredo é usar um nome simples ("API_URL"). 
+# --- A VERIFICAÇÃO CORRETA ---ódios!!
+# 
 # O link real você vai cadastrar lá no painel do Streamlit Cloud.
 API_URL = st.secrets.get("API_URL", os.getenv("API_URL", "http://127.0.0.1:8000/livros"))
 
@@ -51,6 +51,6 @@ if st.button("Atualizar Lista"):
     except Exception as e:
         st.error(f"Não foi possível conectar à API em: {API_URL}")
 
-# Dica para debug (pode apagar depois)
+
 with st.expander("Verificar Conexão"):
     st.write(f"Endereço atual da API: `{API_URL}`")
